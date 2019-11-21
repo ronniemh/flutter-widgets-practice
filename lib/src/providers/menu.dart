666553750d881dc2f24f00e6ab464159012c9497
Ronnie Moncayo <rmoncayo@emagic.fin.ec>
2019-11-21 10:32:31 -0500

    menu

--- lib/src/providers/menu.dart
+++ lib/src/providers/menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/services.dart' show rootBundle;
+import 'dart:convert';
 
 class _MenuProvider {
   List<dynamic> options = [];
@@ -9,7 +10,8 @@ class _MenuProvider {
 
   void _loadData() {
     rootBundle.loadString('data/menu_opts.json').then((data) {
-      print(data);
+      Map dataMap = json.decode(data);
+      print(dataMap);
     });
   }
 }
