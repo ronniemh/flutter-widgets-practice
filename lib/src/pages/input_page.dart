ce8377c6878c5b1638b3de27f07b8c1064639bb5
Ronnie Moncayo <me@ronniemoncayo.dev>
2019-11-21 23:44:05 -0500

    add email and password fields

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -6,7 +6,8 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
-  String _name;
+  String _name = '';
+  String _email = '';
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +17,15 @@ class _InputPageState extends State<InputPage> {
       ),
       body: ListView(
         padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-        children: <Widget>[_crearInput(), Divider(), _createPerson()],
+        children: <Widget>[
+          _crearInput(),
+          Divider(),
+          _createEmail(),
+          Divider(),
+          _createPassword(),
+          Divider(),
+          _createPerson()
+        ],
       ),
     );
   }
@@ -27,7 +36,7 @@ class _InputPageState extends State<InputPage> {
       textCapitalization: TextCapitalization.sentences,
       decoration: InputDecoration(
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
-          counter: Text('Letters ${_name.length}'),
+          counter: Text('Letters ${_name == null ? 'none' : _name.length}'),
           hintText: 'Name',
           labelText: 'Name',
           helperText: 'Only name',
@@ -41,9 +50,44 @@ class _InputPageState extends State<InputPage> {
     );
   }
 
+  Widget _createEmail() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+          hintText: 'Email',
+          labelText: 'Email',
+          suffixIcon: Icon(Icons.alternate_email),
+          icon: Icon(Icons.email)),
+      onChanged: (value) {
+        setState(() {
+          _email = value;
+        });
+      },
+    );
+  }
+
+  Widget _createPassword() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+          hintText: 'Password',
+          labelText: 'Password',
+          suffixIcon: Icon(Icons.lock_open),
+          icon: Icon(Icons.lock)),
+      onChanged: (value) {
+        setState(() {
+          _email = value;
+        });
+      },
+    );
+  }
+
   Widget _createPerson() {
     return ListTile(
       title: Text('Nombre es: $_name'),
+      subtitle: Text('Email: $_email'),
     );
   }
 }
