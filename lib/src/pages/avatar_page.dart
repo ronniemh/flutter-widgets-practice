3cfe81dd7f1e52d5f28fe9660f4eefc50b56ed61
Ronnie Moncayo <me@ronniemoncayo.dev>
2019-11-21 22:23:50 -0500

    added circle avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -8,6 +8,29 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              radius: 25.0,
+              backgroundImage: NetworkImage('https://culto.latercera.com/wp-content/uploads/2019/02/ACDC-2-900x600.jpg'),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('RM'),
+              backgroundColor: Colors.purple,   
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://yt3.ggpht.com/a/AGF-l7-XDzXf0XGgWpk6Ua6FweMINenTFvcWoNholQ=s900-c-k-c0xffffffff-no-rj-mo'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
